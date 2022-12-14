cf96c871583d42f405f50e7d81325d7bee7d59c3
Divyansh <111900596+divyanshddn146@users.noreply.github.com>
2022-12-15 01:48:48 +0530

    Add files via upload

--- Area of traingle/area.c
+++ Area of traingle/area.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+//This is the function used to find area of triangle
 int triangle_area(int b, int h){
 	int area;
 	area=(b*h)/2;
@@ -11,11 +11,11 @@ int triangle_area(int b, int h){
 void main(){
 	int b,h,a;
 	printf("Enter the base of traingle:");
-	scanf("%d",&b);
+	scanf("%d",&b);				//Used to take base as input from user
 	printf("Enter the height of traingle:");
-        scanf("%d",&h);
-	a=triangle_area(b,h);
-	printf("The area of traingle is %d\n",a);
+        scanf("%d",&h);				//Used to take height as input from user
+	a=triangle_area(b,h);				//In this expression we have called the function that we have defined above 
+	printf("The area of traingle is %d\n",a);	//Finally Area of trangle will be printed as output 
 
 
 }
