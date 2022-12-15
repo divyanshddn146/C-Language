e75f86e582e255ca6e13f6b6b49081de36cf0608
Divyansh <111900596+divyanshddn146@users.noreply.github.com>
2022-12-15 11:16:10 +0530

    Add files via upload

--- Armstrong Number/arm.c
+++ Armstrong Number/arm.c
@@ -7,18 +7,18 @@ void is_arm(int x){
 	i=0;
 	y=x;
 	while(y){
-			y=y/10;
-			i++;
-
+			y=y/10;				//This loop is used to get the number of digits of given number that is entered by user	
+			i++;				//As if user enter number 453 then value of i will be 3	
+							//This loop will end when while condition gets false or when value of y=0  
 		
 		}
 	
 	int n,r,sum,c;
 	n=x;
 	sum=0;
-	while(n>0){
-	r=n%10;
-	c=pow(r,i);
+	while(n>0){					//In this loop r will get individual digit of number
+	r=n%10;						//And then it power will be added to sum
+	c=pow(r,i);					//If the value of sum is equal to number entered by user then is an armstrong number else not
 	sum=sum+c;
 	n=n/10;
 	}
