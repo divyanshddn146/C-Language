8b7919b685280586fc8c00c3da8c902837da3ff8
Divyansh <111900596+divyanshddn146@users.noreply.github.com>
2023-01-07 08:06:57 +0530

    Add files via upload

---
+++ LET US C/Chapter 1/Question 3/paper.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(){
+	int a,b,c,i;
+	a=1189;
+	b=841;
+	
+	for(i=0;i<9;i++){
+		
+		
+		printf("The dimension of paper a%d is %d mm * %d mm\n",i,a,b);
+		c=a;
+		a=b;
+		b=c/2;
+		
+		
+
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+}
