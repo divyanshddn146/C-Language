fd34da46d4eb240b9dd742b613aa78558847d16e
Divyansh <111900596+divyanshddn146@users.noreply.github.com>
2023-01-07 07:47:00 +0530

    Add files via upload

---
+++ LET US C/Chapter 1/Question 1/temperature.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+	float f,c;
+	printf("Enter the temperature of city in Fahrenheit degrees:");
+	scanf("%f",&f);
+	c=(5*f-160)/9;
+	printf("The temperture in degree celsius is %f\n",c);
+
+
+
+
+}
